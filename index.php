1165137e522364551a74644588b5f807b3406fc2
jonconst <45972102+jonconst@users.noreply.github.com>
2020-03-23 20:25:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<link href="https://fonts.googleapis.com/css?family=Bellota&display=swap" rel="stylesheet">
+<style>
+body {
+    margin:0;
+    font-family: 'Bellota', cursive;
+}
+.topbar {
+    height: 40px;
+    background-color: #22313f;
+    width: 100%;
+    color:aliceblue;
+    font-family: 'Bellota', cursive;
+    font-size: 30px;
+    padding-left: 200px;
+}
+.content{
+    margin-left:2in;
+}
+</style>
+<div class='topbar'>Axonism</div>
+<div class='content'>
+    <h1>Hello Axonists</h1>
+    <h3>Goals of Axonism</h3>
+    1. Bring out the highest potential out of a Man.<br>
+    2. Free humans from mental slavery.
+</div>
